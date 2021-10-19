df769eaba5fd3ce6de7e1547f0316c4ac120178d
Nathan <nathmon565@gmail.com>
2021-10-19 11:33:22 -0700

    Update ConnectToEV3.m
    Should now connect regardless of file structure...
    But I don't think it works.

--- ConnectToEV3.m
+++ ConnectToEV3.m
@@ -1,12 +1,22 @@
+file = matlab.desktop.editor.getActiveFilename;
+[filePath,fileName,fileExtension] = fileparts(file);
+ev3 = filePath + "\EV3";
+
 try
     brick.playTone(100, 500, 100);
+    disp("Already connected!");
 catch ME
     try
-        javaclasspath("C:\Users\natha\Tools\MATLAB\toolbox\EV3");
+        disp("Connecting to simulation...");
+        javaclasspath(ev3);
+        disp("Loaded java class path...");
+        
+        disp("Connecting via 127.0.0.1...");
         brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5');
         brick.playTone(100, 600, 100);
         pause(1/10);
         brick.playTone(100, 500, 100);
+        disp("Connected!");
         disp("Voltage: " + brick.GetBattVoltage());
     catch ME
         disp("No simulation available. Ensure it's running.");
