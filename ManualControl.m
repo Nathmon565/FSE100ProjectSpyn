0ebd9cf4f4d78632d06f3eb3cc63e6e960740dd6
Nathan <nathmon565@gmail.com>
2021-10-21 12:19:36 -0700

    Change movement to Sprocket
    Up/Down now controls throttle
    Left/Right now controls gear

--- ManualControl.m
+++ ManualControl.m
@@ -1,29 +1,32 @@
+% Motor A: Drive
+% Motor B: Clutch, -1 is forwards
+% Motor C: Claw
+% Input 1: Touch Sensor 1
+% Input 2: Touch Sensor 2
+% Input 3: Color Sensor
+% Input 4: Ultrasonic Sensor
+% Input X: Gyro Sensor
 global key;
 InitKeyboard();
 speed = 30;
+shift = -1;
 while 1
     pause(0.1)
-    leftWheel = 0;
-    rightWheel = 0;
+    throttle = 0;
     switch key
         case 'uparrow'
-            leftWheel = leftWheel + 1;
-            rightWheel = rightWheel + 1;
+            throttle = 1;
         case 'downarrow'
-            leftWheel = leftWheel - 1;
-            rightWheel = rightWheel - 1;
+            throttle = -1;
         case 'leftarrow'
-            leftWheel = leftWheel - 0.5;
-            rightWheel = rightWheel + 0.5;
+            shift = -1;
         case 'rightarrow'
-            leftWheel = leftWheel + 0.5;
-            rightWheel = rightWheel - 0.5;
+            shift = 1;
         case 'q'
             break;
     end
-    brick.MoveMotor('A', rightWheel * speed);
-    brick.MoveMotor('B', leftWheel * speed);
-    disp(leftWheel + ", " + rightWheel);
+    brick.MoveMotor('A', throttle * speed);
+    brick.MoveMotor('B', shift * speed);
 end
 CloseKeyboard();
 clearvars key leftWheel rightWheel speed
