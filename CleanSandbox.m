7abff3f92cb6bfd30514361c9a83083aea7794dc
mdemerso <92818497+mdemerso@users.noreply.github.com>
2021-10-26 12:11:13 -0700

    Update CleanSandbox.m

--- CleanSandbox.m
+++ CleanSandbox.m
@@ -1,1 +1,14 @@
 % this file is for testing code in a clean environment.
+speed = 30;
+shift = -1;
+brick.SetColorMode(3,2);  
+color = brick.ColorCode(3);
+disp (color);
+ pause(0.1)
+    throttle = 0;
+if color == 3
+    brick.MoveMotor('A', 50);
+   % brick.MoveMotor('B', shift * speed);
+    
+end
+    
