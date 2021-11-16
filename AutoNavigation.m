80974574bb20a398d3b9dd3289a997b1af28124b
Nathan <nathmon565@gmail.com>
2021-11-16 12:25:32 -0700

    Update AutoNavigation
    Now displays its current state while traveling.
    Adjust turning values to be as close to 90 degrees as possible.
    Increase back up distance to be as close to half a square as possible.
    Add touch sensor to 3 (now 3 and 4)
    Add color detection for green dropoff and yellow pickup
    DropOff function created
    Increase ultrasonic threshold
    Add beeping when backing up (lol)

--- AutoNavigation.m
+++ AutoNavigation.m
@@ -1,14 +1,14 @@
- DriveForward(brick);
-
-% done
-disp("done");
+DriveForward(brick);
+disp("All done!");
 brick.StopAllMotors();
 brick.beep();
-
+    
 function Shift(brick, drive)
     if(drive)
+        disp("Shifting to drive");
         brick.MoveMotor('D', -25);
     else
+        disp("Shifting to turn");
         brick.MoveMotor('D', 25);
     end
     pause(0.5);
@@ -16,22 +16,27 @@ end
 
 % Shift to turn, drive 90 degrees left, shift to drive
 function TurnLeft(brick)
+    disp("Turning left...");
     Shift(brick, false);
-    brick.MoveMotorAngleRel('A', 70, -360 * 3, 'Brake');
+    brick.MoveMotorAngleRel('A', 70, -360 * 2.9, 'Brake');
     while(brick.MotorBusy('A')); end
+    disp("Next!");
     Shift(brick, true);
 end
 
 % Shift to turn, drive 90 degrees left, shift to drive
 function TurnRight(brick)
+    disp("Turning right...");
     Shift(brick, false);
-    brick.MoveMotorAngleRel('A', 70, 360 * 3, 'Brake');
+    brick.MoveMotorAngleRel('A', 70, 360 * 3.15, 'Brake');
     while(brick.MotorBusy('A')); end
+    disp("Next!");
     Shift(brick, true);
 end
 
 % Drive forward 1 square; check for button press, red strip, wall
 function DriveForward(brick)
+    disp("Driving forward...");
     % whether a stop sign has been witnessed this drive
     seenStopSign = false;
     Shift(brick, true);
@@ -40,7 +45,8 @@ function DriveForward(brick)
     % while busy
     while(brick.MotorBusy('A'))
         % if pressed
-        if(brick.TouchPressed(4))
+        if(brick.TouchPressed(4) || brick.TouchPressed(3))
+            disp("Hit wall!");
             % stop driving
             brick.StopMotor('A', 'Brake');
             BackUpTurnLeft(brick);
@@ -50,31 +56,51 @@ function DriveForward(brick)
         % if red tape
         color = brick.ColorCode(2);
         if(color == 5 && ~seenStopSign)
+            disp("Red! Stop!");
             seenStopSign = true;
             % stop driving
             brick.StopMotor('A', 'Brake');
-            brick.beep();
             % wait at line
             pause(3);
             % drive one half more forward
+            disp("Driving forward 1/2...");
             brick.MoveMotorAngleRel('A', 70, 360 * 10.5, 'Brake');
+        elseif(color == 3) % green dropoff
+            disp("Green!");
+            DropOff(brick);
+            return;
+        elseif(color == 4) % yellow pickup
+            disp("Yellow!");
+            return;
         end
         clearvars color
     end
     
     % check for no wall
-    if(brick.UltrasonicDist(1) > 25)
+    if(brick.UltrasonicDist(1) > 30)
+        disp("No wall!");
         TurnRight(brick);
     end
-    
+    disp("Next!");
     DriveForward(brick);
 end
 
 function BackUpTurnLeft(brick)
+    disp("Backing up!");
     % drive back
-    brick.MoveMotorAngleRel('A', 70, -360 * 5, 'Brake');
-    while(brick.MotorBusy('A')); end
+    brick.MoveMotorAngleRel('A', 70, -360 * 7, 'Brake');
+    while(brick.MotorBusy('A')); BackUpBeeping(brick); end
     Shift(brick, false);
+    disp("Next!");
     % turn left
     TurnLeft(brick);
+end
+
+function BackUpBeeping(brick)
+    brick.beep();
+    pause(0.25);
+end
+
+function DropOff(brick)
+    brick.beep();
 end
